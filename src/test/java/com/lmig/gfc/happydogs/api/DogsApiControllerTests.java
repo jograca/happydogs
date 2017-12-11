2b71c96ca00e75ceb054e1d1c75964587b8053cf
Jon Graca <jon.graca@gmail.com>
2017-12-11 10:50:36 -0500

    Adding first API Controller Test

--- src/test/java/com/lmig/gfc/happydogs/api/DogsApiControllerTests.java
+++ src/test/java/com/lmig/gfc/happydogs/api/DogsApiControllerTests.java
@@ -1,7 +1,26 @@
 package com.lmig.gfc.happydogs.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.lmig.gfc.happydogs.services.DogRepository;
+
 public class DogsApiControllerTests {
 
-	
-	
+	private DogsApiController controller;
+	private DogRepository dogRepo;
+
+	@Before
+	public void setup() {
+		dogRepo = mock(DogRepository.class);
+		controller = new DogsApiController(dogRepo);
+	}
+
+	@Test
+	public void we_will_never_type_this_in_real_life() {
+		assertThat(dogRepo).isNotNull();
+	}
 }
